c8fd36ca83ac91449fb133b35e6eaca3822a98c7
sofiaki <sokiriazi@gmail.com>
2021-12-04 12:57:49 +0200

    added react-snapshot

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { render } from 'react-snapshot';
 
-ReactDOM.render(
   render(<React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
-  )
+  
 );
 
 
